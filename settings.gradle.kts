8da9494e826bbc3898051254109ecdf88b85d720
gudrmsglgl <gudrmsglgl@gmail.com>
2024-06-19 00:24:36 +0900

    setting sub-project

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+rootProject.name = "MyProject"
+
+pluginManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("gradle/plugins")
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("../my-other-project")
+}
+include("app")
+include("business-logic")
+include("data-model")
