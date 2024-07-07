6bbd932c6e2f304e4d76678a7176a12a094b306c
gudrmsglgl <gudrmsglgl@gmail.com>
2024-07-07 17:15:57 +0900

    # 필요에 따른 디펜던시 스코프 다르게 적용

--- business-logic/build.gradle.kts
+++ business-logic/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     implementation(project(":data-model"))
     // group, name, version
     implementation("org.apache.commons:commons-lang3:3.12.0")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:1.7.36")
 }
