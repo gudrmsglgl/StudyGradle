6de550740aab85e670283dbcf13c3d6e6fed527f
gudrmsglgl <gudrmsglgl@gmail.com>
2024-07-13 14:45:18 +0900

    # metadata rule 추가

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -1,6 +1,12 @@
+import com.example.gradle.Slf4jSimpleRule
+
 plugins {
     id("java")
     id("com.diffplug.spotless")
+}
+
+dependencies.components {
+    withModule<Slf4jSimpleRule>("org.slf4j:slf4j-simple")
 }
 
 java {
